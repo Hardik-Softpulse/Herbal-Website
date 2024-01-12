43bebbd1dbc6e45ea3de7c909acbd3a51e8609f4
YashviSoftpulse <softpulsedev15@gmail.com>
2024-01-12 09:58:17 +0530

    homepage

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -17,46 +17,9 @@ export function Hero() {
             modules={[Navigation, Pagination]}
             pagination={{clickable: true}}
             navigation={{clickable: true}}
-          
+            lazy={true}
           >
-            <SwiperSlide lazy="true">
-              <div className="banner_img">
-                <img src={Banner} alt="" loading="lazy"/>
-              </div>
-              <div className="banner_responsive">
-                <img src={BannerResponsive} alt="" loading="lazy"/>
-              </div>
-              <div className="container" loading="lazy">
-                <div className="main_banner_contant flex justify_end">
-                  <div className="banner_contant">
-                    <h4>Herbs that help you</h4>
-                    <h1>Live longer and healthy on planet Earth</h1>
-                    <div className="banner_vector_contant flex">
-                      <div className="banner_vector">
-                        <img src={Banner1} alt="" />
-                        <p>Handmade</p>
-                      </div>
-                      <div className="banner_vector">
-                        <img src={Banner2} alt="" />
-                        <p>Handmade</p>
-                      </div>
-                      <div className="banner_vector">
-                        <img src={Banner3} alt="" />
-                        <p>Handmade</p>
-                      </div>
-                      <div className="banner_vector">
-                        <img src={Banner4} alt="" />
-                        <p>Handmade</p>
-                      </div>
-                    </div>
-                    <a href="#" className="btn">
-                      Shop Now
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide lazy="true">
+            <SwiperSlide >
               <div className="banner_img">
                 <img src={Banner} alt="" loading="lazy"/>
               </div>
@@ -93,7 +56,44 @@ export function Hero() {
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide lazy="true">
+            <SwiperSlide >
+              <div className="banner_img">
+                <img src={Banner} alt="" loading="lazy"/>
+              </div>
+              <div className="banner_responsive">
+                <img src={BannerResponsive} alt="" loading="lazy"/>
+              </div>
+              <div className="container">
+                <div className="main_banner_contant flex justify_end">
+                  <div className="banner_contant">
+                    <h4>Herbs that help you</h4>
+                    <h1>Live longer and healthy on planet Earth</h1>
+                    <div className="banner_vector_contant flex">
+                      <div className="banner_vector">
+                        <img src={Banner1} alt="" />
+                        <p>Handmade</p>
+                      </div>
+                      <div className="banner_vector">
+                        <img src={Banner2} alt="" />
+                        <p>Handmade</p>
+                      </div>
+                      <div className="banner_vector">
+                        <img src={Banner3} alt="" />
+                        <p>Handmade</p>
+                      </div>
+                      <div className="banner_vector">
+                        <img src={Banner4} alt="" />
+                        <p>Handmade</p>
+                      </div>
+                    </div>
+                    <a href="#" className="btn">
+                      Shop Now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide >
               <div className="banner_img">
                 <img src={Banner} alt="" loading="lazy"/>
               </div>
